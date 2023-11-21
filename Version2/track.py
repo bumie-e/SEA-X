ae3b2bfff3c374531cf8ff994f956a7e2f4999a1
bumie <akinremibunmi111@gmail.com>
2023-11-21 12:34:35 +0000

    for testing...

--- Version2/track.py
+++ Version2/track.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 # Load a model
 # model = YOLO('yolov8n.pt')  # load an official detection model
 # model = YOLO('yolov8n-seg.pt')  # load an official segmentation model
-model = YOLO('models/yolov8n.pt')  # load a custom model
+model = YOLO('detect/train4/weights/best.pt')  # load a custom model
 
 # Track with the model
 results = model.track(source="test_vid/dji_export_1655129117122.mp4", show=True, stream=True)
