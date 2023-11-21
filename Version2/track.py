d04947c526936283b8f2093fad521733e0d8f892
bumie <akinremibunmi111@gmail.com>
2023-11-21 12:15:06 +0000

    added modules

---
+++ Version2/track.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+
+# Load a model
+# model = YOLO('yolov8n.pt')  # load an official detection model
+# model = YOLO('yolov8n-seg.pt')  # load an official segmentation model
+model = YOLO('models/yolov8n.pt')  # load a custom model
+
+# Track with the model
+results = model.track(source="test_vid/dji_export_1655129117122.mp4", show=True, stream=True)
+#results = model.track(source="https://youtu.be/LNwODJXcvt4", show=True, tracker="bytetrack.yaml")
--- Version2/train.py
+++ Version2/train.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')
 
 # Train the model using the 'coco128.yaml' dataset for 3 epochs
-results = model.train(data='coco128.yaml', epochs=3)
+results = model.train(data='data/coco128.yaml', epochs=3)
 
 # Evaluate the model's performance on the validation set
 results = model.val()
