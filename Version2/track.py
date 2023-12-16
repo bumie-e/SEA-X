9c70380a651d57307f6db75ad093402eb14fddde
bumie <akinremibunmi111@gmail.com>
2023-12-16 07:26:45 +0000

    5th Training

--- Version2/track.py
+++ Version2/track.py
@@ -3,8 +3,8 @@ from ultralytics import YOLO
 # Load a model
 # model = YOLO('yolov8n.pt')  # load an official detection model
 # model = YOLO('yolov8n-seg.pt')  # load an official segmentation model
-model = YOLO('detect/train4/weights/best.pt')  # load a custom model
+model = YOLO('runs/detect/train5/weights/best.pt')  # load a custom model
 
 # Track with the model
-results = model.track(source="test_vid/dji_export_1655129117122.mp4", show=True, stream=True)
+results = model.track(source="test_vid/dji_export_1655129117122.mp4", show=True)
 #results = model.track(source="https://youtu.be/LNwODJXcvt4", show=True, tracker="bytetrack.yaml")
